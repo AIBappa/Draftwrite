c4f090f450965b44e2618ebd0a8552334e459071
SJ <70745640+AIBappa@users.noreply.github.com>
2026-07-12 16:37:01 +0530

    docker issue update

--- Code/python/ui/__init__.py
+++ Code/python/ui/__init__.py
@@ -1,6 +1,6 @@
 # UI components for Pipeline Author
 from .shell import build_ui, _render_setup_tab, _render_sessions_list
-from .stage1 import render_stage1_prd, render_stage1_full_view, _rebuild_content
+from .stage1 import render_stage1_prd, render_stage1_full_view
 from .stage2 import render_frs_pipeline
 from .stages import render_generic_stage
 
@@ -10,7 +10,6 @@ __all__ = [
     "_render_sessions_list",
     "render_stage1_prd",
     "render_stage1_full_view",
-    "_rebuild_content",
     "render_frs_pipeline",
     "render_generic_stage",
 ]
